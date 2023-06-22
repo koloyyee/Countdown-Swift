0f5c11cc0c03365d0186ab0d82dd4b4191948a0d
Loy Yee Ko <koloyyee@gmail.com>
2023-06-23 06:42:29 +0800

    Initial Commit

---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Loy Yee Ko on 23/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
